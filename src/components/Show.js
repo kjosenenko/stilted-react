c2bf27f30bbbb141f328bf9af7a90e69e8490d28
KJ Osenenko <karljosenenko@gmail.com>
2023-08-20 19:33:06 -0600

    Format show times

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import PresaleLink from './PresaleLink'
+import dayjs from 'dayjs'
 
 function Show(show) {
   const presaleLink = PresaleLink(show.has_presale, show.presale_link)
+  const showDateTime = dayjs(show.show_date_time).format('MM/DD/YYYY hh:mm A')
+
   return (
     <div className="card" key={show.id}>
       <div className="row">
@@ -10,7 +13,7 @@ function Show(show) {
           <div className="card-body">
             <h3>{show.venue}</h3>
             <h5>With {show.supporting_acts}</h5>
-            <h5>{show.show_date} {show.show_time}</h5>
+            <h5>{showDateTime}</h5>
             {presaleLink}
           </div>
         </div>
